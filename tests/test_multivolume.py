59692fbecf41c1e1ece8ac026a06bc29ce617326
Hiroshi Miura <miurahr@linux.com>
2020-09-20 10:28:29 +0900

    Add more api tests
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- tests/test_multivolume.py
+++ tests/test_multivolume.py
@@ -24,7 +24,13 @@ def test_read_check_sha1():
 def test_read_seek():
     target = os.path.join(testdata_path, "archive.7z")
     mv = MV.open(target, mode='rb')
+    assert mv.seekable()
     mv.seek(40000)
+    pos = mv.tell()
+    assert pos == 40000
+    mv.seek(3000)
+    pos = mv.tell()
+    assert pos == 3000
     mv.close()
 
 
@@ -70,8 +76,10 @@ def test_write(tmp_path):
             while len(data) > 0:
                 volume.write(data)
                 data = r.read(BLOCKSIZE)
+        assert volume.seekable()
         volume.seek(0)
         volume.seek(51000)
+        volume.flush()
     created = tmp_path.joinpath('target.7z.0001')
     assert created.exists()
     assert created.stat().st_size == 10240
